0187a6033d0e6c043e1d8561cbd7357927c3d495
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-05 17:23:58 -0800

    location change

--- energy_inv_salic.py
+++ energy_inv_salic.py
@@ -48,7 +48,7 @@ E_target = -13000
 print("max energy is "+str(max(task['E_train'])[0])+'min energy is '+str(min(task['E_train'])[0]))
 print('target is',E_target)
    
-initial = 100
+initial = 319
 print('start from',task["E_train"][initial])
 
     
@@ -89,7 +89,7 @@ n_atom = task['R_train'].shape[1]
 Real_E_record = [task["E_train"][initial][0],new_E[0]*ev_to_kcal]
 Predict_E_record = [task["E_train"][initial][0],E_best]
 Real_loss_record = []
-while n_loop<10:
+while n_loop<15:
     
     n_loop += 1
     print('The '+repr(n_loop)+'-th loop \n')
@@ -109,7 +109,7 @@ while n_loop<10:
     
     
     initial=n_train
-    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.0001,num_step = 20)
+    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.001,num_step = 20)
      
     R_target = Record['R_best']
     E_var_rec =Record['E_var_rec']
