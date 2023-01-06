dd26776f9d94495f209c72ca4aee8b6210bb17c1
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-05 19:49:38 -0800

    update

--- energy_inv_salic.py
+++ energy_inv_salic.py
@@ -111,7 +111,7 @@ while n_loop<15:
     initial=n_train
     #Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.01,num_step = 15)
        
-    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-5,c=0.001,num_step = 10)
+    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-5,c=0.01,num_step = 10)
      
     R_target = Record['R_best']
     E_var_rec =Record['E_var_rec']
