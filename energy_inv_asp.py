cfc4e30550b9e618d4153421e567ccd2a8848808
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-09 17:16:11 -0800

    0.1

--- energy_inv_asp.py
+++ energy_inv_asp.py
@@ -48,11 +48,11 @@ E_target = -16000
 print("max energy is "+str(max(task['E_train'])[0])+'min energy is '+str(min(task['E_train'])[0]))
 print('target is',E_target)
    
-initial = 390
+initial = 190
 print('start from',task["E_train"][initial])
 
     
-Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.1,num_step = 20,random_val = 1e-2)
+Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.1,num_step = 30,random_val = 1e-2)
     
 #Record=gdml_train.inverseE( task1,trained_model,E_target,ind_initial=initial,tol_MAE=0.5,lr=1e-5,c=1,num_step = 10)
 
