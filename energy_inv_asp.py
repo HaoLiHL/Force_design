4d4782ca220bd4c5c23e5e5620028f90912dfec9
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-28 20:45:06 -0800

    update

--- energy_inv_asp.py
+++ energy_inv_asp.py
@@ -48,7 +48,7 @@ E_target = -17700
 print("max energy is "+str(max(task['E_train'])[0])+'min energy is '+str(min(task['E_train'])[0]))
 print('target is',E_target)
    
-initial = 4
+initial = 1
 print('start from',task["E_train"][initial])
 
     
