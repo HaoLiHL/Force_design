f4c6327df96b458b2f4c63e085ae7b304c1321f0
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-28 20:09:32 -0800

    add

--- energy_inv_asp.py
+++ energy_inv_asp.py
@@ -48,7 +48,7 @@ E_target = -17700
 print("max energy is "+str(max(task['E_train'])[0])+'min energy is '+str(min(task['E_train'])[0]))
 print('target is',E_target)
    
-initial = 300
+initial = 30
 print('start from',task["E_train"][initial])
 
     
@@ -111,7 +111,7 @@ while n_loop<10:
     initial=n_train
     #Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.01,num_step = 15)
        
-    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=0.1,num_step = 30,random_val = 1e-2)
+    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=0.8,num_step = 30,random_val = 1e-2)
      
     R_target = Record['R_best']
     E_var_rec =Record['E_var_rec']
