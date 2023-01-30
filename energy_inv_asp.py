f0ba5a580ca7095015942df00a9df346433678d4
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-01-30 12:11:23 -0800

    a

--- energy_inv_asp.py
+++ energy_inv_asp.py
@@ -52,7 +52,7 @@ initial = 37
 print('start from',task["E_train"][initial])
 
     
-Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=8,num_step = 5,random_val = 1e-4)
+Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=2,num_step = 5,random_val = 1e-2)
     
 #Record=gdml_train.inverseE( task1,trained_model,E_target,ind_initial=initial,tol_MAE=0.5,lr=1e-5,c=1,num_step = 10)
 
@@ -111,7 +111,7 @@ while n_loop<30:
     initial=n_train
     #Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-3,c=0.01,num_step = 15)
        
-    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=8,num_step = 30,random_val = 1e-4)
+    Record=AFF_train.inverseE_new( task,trained_model,E_target,ind_initial=initial,tol_MAE=0.01,lr=1e-1,c=2,num_step = 30,random_val = 1e-2)
      
     R_target = Record['R_best']
     E_var_rec =Record['E_var_rec']
