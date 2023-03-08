18af0463c7ebe0a0f2676392cac3e1666bcd5698
HaoLiHL <88747303+HaoLiHL@users.noreply.github.com>
2023-03-07 21:58:53 -0800

    update

--- utils/tensor_aff.py
+++ utils/tensor_aff.py
@@ -1631,7 +1631,7 @@ class GDMLTrain(object):
         
         
     
-    def inverse(self,task,trained_model,initial=0,c = 1, n_iter = 30,random_noise = 1e-2):
+    def inverse(self,task,trained_model,initial=0,c = 1, n_iter = 30,random_noise = 1e-2, step_size = 1e-12):
         
         sig_optim= trained_model['sig_optim']
         alphas_opt= trained_model['alphas_opt']
@@ -1680,7 +1680,7 @@ class GDMLTrain(object):
         R_val_tensor.requires_grad_()
         optimizer = torch.optim.Adam([R_val_tensor], lr=1e-3)
         #optimizer = torch.optim.SGD([R_val_tensor], lr=1e-4, momentum=0.9)
-        step_size = 1e-12
+        #step_size = 1e-12
         for index in range(n_iter):
             optimizer.zero_grad()
             
